f3acf8149aa050043f7f39a77bce0c82079de153
Ernest <erntye@hotmail.com>
2020-01-20 18:40:37 +0800

    added export script

---
+++ export.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export ARM_CLIENT_ID="909eda55-560e-4383-8f68-dba83567b2c1"
+export ARM_CLIENT_SECRET="C/5n.x8rvWsidJ14?WQphd.8:L7_nQQz"
+export ARM_SUBSCRIPTION_ID="ba303b42-79ed-4f1c-a283-6724f3a23b07}"
+export ARM_TENANT_ID="d2e87a20-448f-4835-8a5f-daba2d151414"
