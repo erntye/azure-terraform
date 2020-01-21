a6d872ded179a9865355659086b0ec214483ecdf
Ernest <erntye@hotmail.com>
2020-01-21 09:43:19 +0800

    create big main

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "azurerm" {
 }
 
 module "resource-group" {
-  source = "modules/create-resource-group"
+  source = "./modules/create-resource-group"
 }
 
 module "app-service"  {
-  source  = "modules/create-app-service"
+  source  = "./modules/create-app-service"
   # insert the 6 required variables here
 } 
