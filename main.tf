f2a0b3f86bea9a257a41702a2b199000f82573b7
Ernest <erntye@hotmail.com>
2020-01-21 09:57:03 +0800

    create big main

--- main.tf
+++ main.tf
@@ -3,9 +3,17 @@ provider "azurerm" {
 
 module "resource-group" {
   source = "./modules/create-resource-group"
+  name                = var.name
 }
 
 module "app-service"  {
   source  = "./modules/create-app-service"
-  # insert the 6 required variables here
+  name                = var.name
+  location            = var.rg_location
+  resource_group_name = var.name
+  tier = var.tier
+  size = var.size
+  app_settings        = var.app_settings
+  https_only          = var.https_only
+  always_on = var.always_on
 } 
