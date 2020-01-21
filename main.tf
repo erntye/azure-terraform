bc533c0b780982e5ea5da7e213b68dff689ae159
Ernest <erntye@hotmail.com>
2020-01-21 09:41:14 +0800

    create big main

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 provider "azurerm" {
 }
 
-module "app-service" createas {
+module "resource-group {
+  source = "modules/create-resource-group"
+}
+
+module "app-service"  {
   source  = "modules/create-app-service"
   # insert the 6 required variables here
 } 
--- modules/create-resource-group/main.tf
+++ modules/create-resource-group/main.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "rg" {
-        name = "${var.name}RG"
-        location = "Southeast Asia"
+  name = "${var.name}RG"
+  location = "Southeast Asia"
 }
